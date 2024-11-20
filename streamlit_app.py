4c9bcc19116d612c61330dfe826667e68d880bbd
Nilawahyusaputri <110802059+Nilawahyusaputri@users.noreply.github.com>
2024-11-21 02:02:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,1 @@
-<form id="upload-form">
-    <input type="file" id="file-input" name="file" accept="image/*">
-    <button type="submit">Deteksi Glaukoma</button>
-</form>
-<div id="result"></div>
 
-<script>
-    document.getElementById('upload-form').onsubmit = async (e) => {
-        e.preventDefault();
-        let fileInput = document.getElementById('file-input');
-        let formData = new FormData();
-        formData.append('file', fileInput.files[0]);
-
-        let response = await fetch('/predict', { method: 'POST', body: formData });
-        let result = await response.json();
-        document.getElementById('result').innerText = 'Hasil: ' + result.prediction;
-    };
-</script>
